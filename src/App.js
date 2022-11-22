3aa8c41be6e135d62f1edfa4af1ea15ded6cedc8
Nate Ensign <dixienate@gmail.com>
2022-11-22 16:06:50 -0700

    week 1 code along with brady

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const name = "Nate"
+  return (
+    <div className="App">
+     <h1>Creating & Using Components</h1>
+     <h2>Value: {name} </h2>
+    </div>
+  );
+}
+
+export default App;
